ab4af54b74ad632004700a5fbe35866eb22bae26
Martin Dallos <tinypin14@gmail.com>
2023-01-10 13:46:22 +0100

    Finished content creation and basic layout and styling

--- src/contactPageElems.js
+++ src/contactPageElems.js
@@ -14,16 +14,42 @@ export const contactPageElements = {
     main: `
     <main>
             <div class="main-inner">
-                <div class="contact card">
-                    <p>contactTest</p>
+                <div class="contact-item address card">
+                    <h2>Address</h2>
+                    <p>The restaurant is located in a bustling city center, easily accessible by public transportation or car. If you're taking the subway, get off at the main station and exit towards main street, from there walk 3 blocks north, the restaurant will be on your left. If you're driving, there is a parking garage located on the next block and street parking available in the area. Once you've arrived, look for the big sign with the restaurant name, you can't miss it! The restaurant is surrounded by plenty of other dining and shopping options, so feel free to make a day of it. Don't forget to make a reservation to ensure a table is available.</p>
+                    <img src="./../dist/sincerely-media-OgSkCgSfEic-unsplash.jpg" alt="Our restaurant" height="75" width="75">
                 </div>
+                <div class="contact-item contact-method card">
+                    <h2>How to contact us:</h2>
+                    <ul>
+                        <li>Address: 123 Main Street
+                        Anytown, USA 00000</li>
+                        <li>Email: info@morningmashup.com</li>
+                        <li>Telephone: +012 3434567</li>
+                        <li>Best method: <strong>Come, visit us!</strong></li>
+                    </ul>
+                </div>
+                <div class="contact-item owner-intro card">
+                    <h2>Who are we?</h2>
+                    <p>Meet John and Sarah, the proud owners of Morning Mash-Up, a cozy brunch spot located in the heart of the city. With a passion for delicious food and great service, they have created a warm and inviting atmosphere where friends and family can gather to enjoy a delicious meal and good company. John's expertise in the kitchen, combined with Sarah's eye for design, has resulted in a restaurant that is not only delicious, but also beautiful. Together they have created a menu that offers a unique twist on traditional brunch favorites, using the freshest ingredients and the latest cooking techniques. They look forward to welcoming you to Morning Mash-Up and providing you with an unforgettable dining experience.</p>
+                    <img src="./../dist/bruce-mars-ieIY61ZHhs8-unsplash.jpg" alt="The owners" height="75" width="75">
+                </div>
+
             </div>
     </main>
     `,
     footer: `
     <footer>
         <p>Created by Martin Dallos</p>
-        <p>Photo by <a href="https://unsplash.com/@impatrickt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Patrick Tomasso</a> on <a href="https://unsplash.com/photos/GXXYkSwndP4?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
+        <p>
+            Photo by <a href="https://unsplash.com/@impatrickt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Patrick Tomasso</a> on <a href="https://unsplash.com/photos/GXXYkSwndP4?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        </p>
+        <p>
+            Photo by <a href="https://unsplash.com/@sincerelymedia?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sincerely Media</a> on <a href="https://unsplash.com/photos/OgSkCgSfEic?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        </p>
+        <p>
+            Photo by <a href="https://unsplash.com/es/@brucemars?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">bruce mars</a> on <a href="https://unsplash.com/photos/ieIY61ZHhs8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        </p>
     </footer>
 
     `
--- src/menuPageElems.js
+++ src/menuPageElems.js
@@ -17,8 +17,26 @@ export const menuPageElements = {
                 <div class="menu-item card">
                     <h2>Cappuccino</h2>
                     <p>A classic Italian coffee drink made with equal parts espresso, steamed milk, and foam. Perfectly balanced and topped with a sprinkle of cocoa powder for a touch of sweetness.</p>
-                    <h3 class="price">2 EUR</h3>
+                    <h3 class="price">Price: 2 EUR</h3>
                     <img src="./../dist/anubhav-arora-RFLDagtOsMM-unsplash.jpg" alt="A cup of cappuccino" height="75" width="75">
+                </div>
+                <div class="menu-item card">
+                    <h2>Cortado</h2>
+                    <p>A Spanish coffee drink made with equal parts espresso and steamed milk. A touch of foam gives it a creamy texture, while the small amount of milk makes it a less sweet and less creamy than a latte. Perfect for those who appreciate a strong coffee taste but also want a smooth texture.</p>
+                    <h3 class="price">Price: 1.5 EUR</h3>
+                    <img src="./../dist/ben-moreland-eSzClaMXNkk-unsplash.jpg" alt="A cup of cortado" height="75" width="75">
+                </div>
+                <div class="menu-item card">
+                    <h2>Croissant</h2>
+                    <p>A traditional French pastry made with flaky, buttery layers of puff pastry. Crispy on the outside and soft on the inside, our croissant are made fresh daily with delicate layers of butter and flour, perfect to have it on its own or filled with chocolate, almond paste, or jam. An indulgent breakfast option or a satisfying snack any time of day.</p>
+                    <h3 class="price">Price: 1 EUR</h3>
+                    <img src="./../dist/mahyar-motebassem-123wTsGP4LU-unsplash.jpg" alt="Six croissants on a baking pan" height="75" width="75">
+                </div>
+                <div class="menu-item card">
+                    <h2>Bagel</h2>
+                    <p>A classic combination of flavors on a crispy and chewy everything bagel. Thinly sliced, deli-style ham is piled high and topped with your choice of melted cheese. Served warm, it's perfect for a hearty and satisfying breakfast or lunch. Add some lettuce and tomato for a extra crunch and some mustard or mayo to complete this delicious sandwich.</p>
+                    <h3 class="price">Price: .75 EUR</h3>
+                    <img src="./../dist/vicky-ng-kmX09a309gI-unsplash.jpg" alt="A bagel filled with ham, cheese and lettuce" height="75" width="75">
                 </div>
             </div>
     </main>
@@ -27,6 +45,14 @@ export const menuPageElements = {
     <footer>
         <p>Created by Martin Dallos</p>
         <p>Photo by <a href="https://unsplash.com/@impatrickt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Patrick Tomasso</a> on <a href="https://unsplash.com/photos/GXXYkSwndP4?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
+        <p>Photo by <a href="https://unsplash.com/@relentlessjpg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ben Moreland</a> on <a href="https://unsplash.com/photos/eSzClaMXNkk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        </p>
+        <p>
+             Photo by <a href="https://unsplash.com/@mahyarmotebassem?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">mahyar motebassem</a> on <a href="https://unsplash.com/photos/123wTsGP4LU?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        </p>
+        <p>
+            Photo by <a href="https://unsplash.com/es/@vickyng?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Vicky Ng</a> on <a href="https://unsplash.com/photos/kmX09a309gI?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        </p>
     </footer>
 
     `
